4fb7e02f32d7a6b8aff3f74539e032933d3a0614
ricardoinaciopt <ricardo.inacio.2002@gmail.com>
2023-07-27 22:47:25 +0100

    javascript bug fixes

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
 //about
 const images = document.getElementsByClassName("image");
 
-let globalIndex = 0,
-  last = { x: 0, y: 0 };
+if (images.length != 0) {
+  let globalIndex = 0,
+    last = { x: 0, y: 0 };
 
-const activate = (image, x, y) => {
-  image.style.left = `${x}px`;
-  image.style.top = `${y}px`;
-  image.style.zIndex = globalIndex;
+  const activate = (image, x, y) => {
+    image.style.left = `${x}px`;
+    image.style.top = `${y}px`;
+    image.style.zIndex = globalIndex;
 
-  image.dataset.status = "active";
+    image.dataset.status = "active";
 
-  last = { x, y };
-};
+    last = { x, y };
+  };
 
-const distanceFromLast = (x, y) => {
-  return Math.hypot(x - last.x, y - last.y);
-};
+  const distanceFromLast = (x, y) => {
+    return Math.hypot(x - last.x, y - last.y);
+  };
 
-const handleOnMove = (e) => {
-  if (distanceFromLast(e.clientX, e.clientY) > window.innerWidth / 20) {
-    const lead = images[globalIndex % images.length],
-      tail = images[(globalIndex - 5) % images.length];
+  const handleOnMove = (e) => {
+    if (distanceFromLast(e.clientX, e.clientY) > window.innerWidth / 20) {
+      const lead = images[globalIndex % images.length],
+        tail = images[(globalIndex - 5) % images.length];
 
-    activate(lead, e.clientX, e.clientY);
+      activate(lead, e.clientX, e.clientY);
 
-    if (tail) tail.dataset.status = "inactive";
+      if (tail) tail.dataset.status = "inactive";
 
-    globalIndex++;
-  }
-};
+      globalIndex++;
+    }
+  };
 
-window.onmousemove = (e) => handleOnMove(e);
-
-window.ontouchmove = (e) => handleOnMove(e.touches[0]);
+  window.onmousemove = (e) => handleOnMove(e);
+  window.ontouchmove = (e) => handleOnMove(e.touches[0]);
+}
 const ano = new Date().getFullYear();
 document.getElementById("ano").textContent = ano;

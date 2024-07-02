ab334ae9a7b0f4c3bf84eb43102631af4f0675cd
Ricardo Inácio <ricardo.inacio.2002@gmail.com>
2024-07-02 15:20:08 +0100

    new home page

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+//home
+
+const ascii_container = document.getElementById("ascii-container");
+let index = 0;
+let asciiArt = "";
+
+function animateAscii() {
+  if (index < asciiArt.length) {
+    if (asciiArt[index] === "\n") {
+      ascii_container.innerHTML += "<br>";
+    } else {
+      ascii_container.innerHTML += asciiArt[index];
+    }
+    index++;
+    setTimeout(animateAscii, 0.001);
+  }
+}
+
+window.onload = function () {
+  fetch("/assets/me.txt")
+    .then((response) => response.text())
+    .then((text) => {
+      asciiArt = text.trim().replace(/^\s+/, "");
+      animateAscii();
+    })
+    .catch((error) => console.error("Error fetching the text file:", error));
+};
+
 //about
 const images = document.getElementsByClassName("image");
 
